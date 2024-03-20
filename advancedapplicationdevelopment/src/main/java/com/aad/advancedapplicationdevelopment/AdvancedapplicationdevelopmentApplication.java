faf19fc349a52c57174ce65a3707ba9bc57322f6
Siva Dhayanithi <110531878+Dayanithi964@users.noreply.github.com>
2024-03-20 16:13:55 +0530

    Day6-BackendSetup

---
+++ advancedapplicationdevelopment/src/main/java/com/aad/advancedapplicationdevelopment/AdvancedapplicationdevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.aad.advancedapplicationdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedapplicationdevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedapplicationdevelopmentApplication.class, args);
+	}
+
+}
---
+++ advancedapplicationdevelopment/src/test/java/com/aad/advancedapplicationdevelopment/AdvancedapplicationdevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aad.advancedapplicationdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedapplicationdevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
